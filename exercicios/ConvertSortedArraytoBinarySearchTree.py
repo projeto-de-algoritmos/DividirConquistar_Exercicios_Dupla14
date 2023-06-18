9cfb9a3dec4aa995735c567a8b0d535c74df092f
henriqtorresl <henriquetlandin@gmail.com>
2023-06-18 20:03:35 -0300

    adicionando a solução do exercício

--- exercicios/ConvertSortedArraytoBinarySearchTree.py
+++ exercicios/ConvertSortedArraytoBinarySearchTree.py
@@ -1,1 +1,20 @@
-# adicioanr o exercicio...
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+
+    def sortedArrayToBST(self, nums):   # converte um array ordenado em uma árvore binária de busca...
+
+        if not nums:
+            return None
+        
+        meio = len(nums) // 2       # obtendo apenas o quociente inteiro da divisão
+        
+        raiz = TreeNode(nums[meio])
+        raiz.left = self.sortedArrayToBST(nums[:meio])
+        raiz.right = self.sortedArrayToBST(nums[meio+1:])
+        
+        return raiz
